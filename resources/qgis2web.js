4ae5a6ccc07bc88ab61ee604e5b688730d53c037
Archeoisio <m.musio@yahoo.it>
2026-06-18 15:43:49 +0200

    Update qgis2web.js

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -5,7 +5,7 @@
 var centroMappa = [1400000.000000, 5200000.000000]; 
 
 // Scegli qui lo zoom iniziale libero per i due dispositivi:
-var zoomDesktopScelto = 6.0;  // Valore ottimale per PC
+var zoomDesktopScelto = 5.8;  // Valore ottimale per PC
 var zoomMobileScelto  = 4.8;  // Valore ottimale per Smartphone/Tablet
 
 var isSmallScreen = window.innerWidth < 650;
